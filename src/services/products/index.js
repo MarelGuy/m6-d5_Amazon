919592337bda1619711cb3cf5caad7f255a9cfe9
Loris Cuntreri <62082748+MarelGuy@users.noreply.github.com>
2021-01-15 12:30:17 +0100

    finished and polished endpoints

--- src/services/products/index.js
+++ src/services/products/index.js
@@ -1,29 +1,122 @@
 const express = require("express");
 const mongoose = require("mongoose")
 
+const multer = require("multer")
+const { CloudinaryStorage } = require("multer-storage-cloudinary")
+const cloudinary = require("../../config/cloudinary")
+
 const productModel = require("../../schemas/product_schema")
 
 const app = express.Router()
 
-app.get('/', (req, res) => {
+const storage = new CloudinaryStorage({
+    cloudinary: cloudinary,
+    params: {
+        folder: process.env.FOLDER_NAME
+    }
+})
+const multerCloudinary = multer({
+    "storage": storage
+})
 
+app.get('/', async (req, res, next) => {
+    try {
+        const products = await productModel.find().populate("reviews")
+        if (products.length >= 1) {
+            res.status(200).send(products)
+            console.log("\x1b[32m", "Someone did GET/products")
+        } else {
+            res.status(404).send("404, not found. Could it be that there aren't any products in the databse?")
+            console.log('\x1b[33m%s\x1b[0m', "Someone did GET/products but encountered 404")
+        }
+    } catch (err) {
+        console.log("\x1b[31m", err)
+        next(err)
+    }
 });
 
-app.get('/:id', (req, res) => {
-
+app.get('/:id', async (req, res, next) => {
+    try {
+        const product = await productModel.findById(req.params.id).populate("reviews")
+        if (product) {
+            res.status(200).send(product)
+            console.log("\x1b[32m", "Someone did GET/products/" + req.params.id)
+        } else {
+            res.status(404).send("404, not found. Wrong id or the elemnt doesn't exist?")
+            console.log('\x1b[33m%s\x1b[0m', " Someone did GET/products/" + req.params.id + " but encountered 404")
+        }
+    } catch (err) {
+        console.log("\x1b[31m", err)
+        next(err)
+    }
 });
 
-app.post('/', (req, res) => {
-
+app.post('/', async (req, res, next) => {
+    try {
+        const newProduct = new productModel(req.body)
+        var { _id } = await newProduct.save()
+        if (_id) {
+            res.status(200).send("Created! here's the id of the product: " + _id)
+            console.log("\x1b[32m", "Someone created a product. Id: " + _id)
+        } else {
+            res.send("Unknown error")
+            console.log('\x1b[33m%s\x1b[0m', "Someone tried to create a product but encountered an error")
+            next()
+        }
+    } catch (err) {
+        console.log("\x1b[31m", err)
+        next(err)
+    }
 });
 
-app.put('/:id', (req, res) => {
+app.post("/:id/imageupload", multerCloudinary.single("imageUrl"), async (req, res, next) => {
+    try {
+        console.log("\x1b[32m", "Someone uploaded an image on product id: " + req.params.id)
+        res.status(201).send("Image uploaded on product id:" + req.params.id)
+    } catch (err) {
+        console.log("\x1b[31m", err)
+        next(err)
+    }
+})
 
+app.put('/:id', async (req, res, next) => {
+    try {
+        const modifiedProduct = await productModel.findByIdAndUpdate(req.params.id, req.body, {
+            runValidators: true,
+            new: true
+        })
+        console.log()
+        if (modifiedProduct) {
+            res.status(200).send("Product changed, new id:" + modifiedProduct._id)
+            console.log("\x1b[32m", "Someone modified a product, id: " + req.params.id)
+        } else {
+            res.send("Unknown error, maybe you mistaken the id?")
+            console.log('\x1b[33m%s\x1b[0m', "Someone tried to modify a product but encountered an error, id: " + req.params.id)
+            next()
+        }
+    } catch (err) {
+        console.log("\x1b[31m", err)
+        next(err)
+    }
 });
 
-app.delete('/:id', (req, res) => {
-
+app.delete('/:id', async (req, res, next) => {
+    try {
+        const DeletedProduct = await productModel.findByIdAndDelete(req.params.id)
+        if (DeletedProduct) {
+            res.status(200).send("Product deleted!")
+            console.log("\x1b[32m", "Someone deleted a product, id: " + req.params.id)
+        } else {
+            res.send("Unknown error, maybe you mistaken the id?")
+            console.log('\x1b[33m%s\x1b[0m', "Someone tried to delete a product but encountered an error, id: " + req.params.id)
+            next()
+        }
+    } catch (err) {
+        console.log("\x1b[31m", err)
+        next(err)
+    }
 });
+
 
 
 module.exports = app
--- src/services/reviews/index.js
+++ src/services/reviews/index.js
@@ -1,28 +1,107 @@
 const express = require("express");
 const mongoose = require("mongoose")
 
-const productModel = require("../../schemas/product_schema")
+const reviewsModel = require("../../schemas/reviews_schema")
 
 const app = express.Router()
 
-app.get('/', (req, res) => {
-
+app.get('/', async (req, res, next) => {
+    try {
+        const reviews = await reviewsModel.find()
+        if (reviews.length >= 1) {
+            res.status(200).send(reviews)
+            console.log("\x1b[32m", "Someone did GET/reviews")
+        } else {
+            res.status(404).send("404, not found. Could it be that there aren't any reviews in the databse?")
+            console.log('\x1b[33m%s\x1b[0m', "Someone did GET/reviews but encountered 404")
+        }
+    } catch (err) {
+        console.log("\x1b[31m", err)
+        next(err)
+    }
 });
-
-app.get('/:id', (req, res) => {
-
+app.get('/:id', async (req, res, next) => {
+    try {
+        const review = await reviewsModel.findById(req.params.id)
+        if (review) {
+            res.status(200).send(review)
+            console.log("\x1b[32m", "Someone did GET/reviews/" + req.params.id)
+        } else {
+            res.status(404).send("404, not found. Wrong id or the elemnt doesn't exist?")
+            console.log('\x1b[33m%s\x1b[0m', " Someone did GET/reviews/" + req.params.id + " but encountered 404")
+        }
+    } catch (err) {
+        console.log("\x1b[31m", err)
+        next(err)
+    }
 });
-
-app.post('/', (req, res) => {
-
+app.post('/', async (req, res, next) => {
+    try {
+        try {
+            const newReview = new reviewsModel(req.body)
+            var { _id } = await newReview.save()
+            if (_id) {
+                res.status(200).send("Created! here's the id of the review: " + _id)
+                console.log("\x1b[32m", "Someone created a review. Id: " + _id)
+            } else {
+                res.send("Unknown error")
+                console.log('\x1b[33m%s\x1b[0m', "Someone tried to create a review but encountered an error")
+                next()
+            }
+        } catch (err) {
+            console.log("\x1b[31m", err)
+            next(err)
+        }
+    } catch (err) {
+        console.log(err)
+        next(err)
+    }
 });
-
-app.put('/:id', (req, res) => {
-
+app.put('/:id', async (req, res, next) => {
+    try {
+        try {
+            const modifiedReview = await reviewsModel.findByIdAndUpdate(req.params.id, req.body, {
+                runValidators: true,
+                new: true
+            })
+            console.log()
+            if (modifiedReview) {
+                res.status(200).send("Review changed, new id:" + modifiedReview._id)
+                console.log("\x1b[32m", "Someone modified a review, id: " + req.params.id)
+            } else {
+                res.send("Unknown error, maybe you mistaken the id?")
+                console.log('\x1b[33m%s\x1b[0m', "Someone tried to modify a review but encountered an error, id: " + req.params.id)
+                next()
+            }
+        } catch (err) {
+            console.log("\x1b[31m", err)
+            next(err)
+        }
+    } catch (err) {
+        console.log(err)
+        next(err)
+    }
 });
-
-app.delete('/:id', (req, res) => {
-
+app.delete('/:id', async (req, res, next) => {
+    try {
+        try {
+            const DeletedReview = await reviewsModel.findByIdAndDelete(req.params.id)
+            if (DeletedReview) {
+                res.status(200).send("Review deleted!")
+                console.log("\x1b[32m", "Someone deleted a review, id: " + req.params.id)
+            } else {
+                res.send("Unknown error, maybe you mistaken the id?")
+                console.log('\x1b[33m%s\x1b[0m', "Someone tried to delete a review but encountered an error, id: " + req.params.id)
+                next()
+            }
+        } catch (err) {
+            console.log("\x1b[31m", err)
+            next(err)
+        }
+    } catch (err) {
+        console.log(err)
+        next(err)
+    }
 });
 
 
