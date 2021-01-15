377e2158dffb37e6885ad4f3fdb320b7c31510d4
Loris Cuntreri <62082748+MarelGuy@users.noreply.github.com>
2021-01-15 10:19:53 +0100

    added base CRUD for products endpoint

---
+++ src/services/products/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const mongoose = require("mongoose")
+
+const productModel = require("../../schemas/product_schema")
+
+const app = express.Router()
+
+app.get('/', (req, res) => {
+
+});
+
+app.get('/:id', (req, res) => {
+
+});
+
+app.post('/', (req, res) => {
+
+});
+
+app.put('/:id', (req, res) => {
+
+});
+
+app.delete('/:id', (req, res) => {
+
+});
+
+
+module.exports = app
